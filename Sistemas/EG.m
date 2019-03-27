ff2d65b5abb7275c3c4d3bc7afd316df3a95d9b7
PauloHenriqueRCS <paulohenriquercs@gmail.com>
2019-03-26 22:17:01 -0300

    Gauss on

--- Sistemas/EG.m
+++ Sistemas/EG.m
@@ -1,9 +1,11 @@
-function x = EG (A,b)
+function [A] = EG (A,b)
   n = size(A,1);
   Au = [A b];
-  for i = 2 : 1 : n - 1
-   for j = 1 : 1 : n - 1
-      Au(i,j) = (Au(j,i)/Au(j-1,i)*-1) * Au(j-1,:) + Au(j,:);
+    for j = 1 : n - 1
+      for i = j : n - 1
+      Au(i + 1,:) = -(Au(i + 1, j) / Au(j, j)) * Au(j, :) + Au(i + 1, :);
    endfor
   endfor
+  A = Au;
+  
 endfunction
