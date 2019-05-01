2166dd6c52031f5afda58b00aef866b59a0b4e73
Paulo <paulohenriquercs@gmail.com>
2019-05-01 11:15:55 -0300

    Ajustes, tudo funcionando

--- Sistemas/PLagrange.m
+++ Sistemas/PLagrange.m
@@ -1,5 +1,6 @@
 function [P] = PLagrange(x,y,p)
-n = length(p);
+  p
+n = length(p)
 m = length(x);
 for k = 1 : n
 r = 0;
@@ -7,7 +8,7 @@ r = 0;
     c = 1;
     d = 1;
       for j = 1 : m
-        if i ~= j
+        if i != j
           c = c * (p(k) - x(j));
           d = d * (x(i) - x(j));
         endif
--- Sistemas/PNewton.m
+++ Sistemas/PNewton.m
@@ -1,37 +1,23 @@
-%{
-for i = 1 : n - 1
-y1(i) = (y(i+1)-y(i))/(x(i+1)-x(i));
-endfor
-
-for i = 1 : n - 2
-y2(i) = ((y1(i+1)-y1(i))/(x(i+2)-x(i)))();
-endfor
-
-for i = 1 : n - 3
-y3(i) = ((y2(i+1)-y2(i))/(x(i+3)-x(i)));
-endfor
-%}
-
-
 function [P] = PNewton(x,y,p)
 m = length(x);
 n = length(p);
 
-for t = 1 : n
-for i = 1 : m
-  Dely(i) = y(i);
-endfor
-
-for k = 1 : m-1
-  for i = m : -1 : k+1
-  Dely(i) = (Dely(i) - Dely(i-1))/(x(i)-x(i-k));
+  for t = 1 : n
+    
+    for i = 1 : m
+      Dely(i) = y(i);
+    endfor
+  
+    for k = 1 : m-1
+      for i = m : -1 : k+1
+        Dely(i) = (Dely(i) - Dely(i-1))/(x(i)-x(i-k));
+      endfor
+      P(t) = Dely(m);
+    endfor
+  
+    for i = m-1 : -1 : 1
+      P(t) = P(t)*(p(t)-x(i))+Dely(i);
+    endfor
+  
   endfor
-  P(t) = Dely(m);
-endfor
-
-for i = m-1 : -1 : 1
-P(t) = P*(p(t)-x(i))+Dely(i);
-endfor
-
-endfor
 endfunction
