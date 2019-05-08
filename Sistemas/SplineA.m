d549f78106e795139cd8a1f841b0be9c05f933a5
aluno <aluno>
2019-05-08 08:18:57 -0300

    Spline

---
+++ Sistemas/SplineA.m
@@ -0,0 +1,26 @@
+function [sz ,CondErro ,aux] = SplineA(n,x,y,m,z)
+[s2,CondErro]= SplineN(n,x,y);
+if CondErro~=0, return , end , CondErro =0; aux = [];
+for j=1:m
+if z(j)>=x(1)&z(j)<=x(n)
+inf = 1;
+sup=n;
+while((sup-inf)>1)
+ind=round((inf+sup)/2);
+if x(ind)>z(j), sup=ind;
+ else inf=ind;
+ end
+end
+h=x(sup)-x(inf);
+a=(s2(sup)-s2(inf))/(6*h);
+b=s2(inf)*.5;
+c=(y(sup)-y(inf))/h-(s2(sup)+2*s2(inf))*h/6;
+d=y(inf); 
+h=z(j)-x(inf);
+sz(j)=((a*h+b)*h+c)*h+d;
+aux=[aux;inf-1,a,b,c,d,z(j),sz(j)];
+else
+sz(j)=0; CondErro=CondErro +1;
+end
+end
+end
---
+++ Sistemas/SplineN.m
@@ -0,0 +1,35 @@
+function [s2,CondErro]= SplineN(n ,x ,y)
+
+if n<3, CondErro = 1; 
+  return;
+endif; 
+
+CondErro = 0;
+m = n−2; 
+Ha = x(2)-x(1);
+Deltaa =(y(2)-y(1))/Ha;
+
+for i=1:m
+  Hb = x(i+2)-x(i+1); 
+  Deltab = (y(i+2)-y(i+1))/Hb;
+  e(i) = Hb ;
+  d(i) = 2*(Ha+Hb );
+  s2(i+1)=6*(Deltab-Deltaa);
+  Ha = Hb; 
+  Deltaa = Deltab;
+endfor
+
+for i=2:m
+  t = e(i−1)/d(i−1); 
+  d(i) = d(i)-t*e(i−1);
+  s2(i+1) = s2(i+1)-t*s2(i);
+endfor
+
+s2(m+1)= s2(m+1)/d(m);
+for i=m:-1:2 ,s2(i)=(s2(i)-e(i-1)*s2(i+1))/d(i-1); 
+endfor
+
+s2(1) = 0;
+s2(m+2) = 0;
+
+endfunction
