833d28aa49e7f4c9180dc253282fc792184ef5c0
Paulo <paulohenriquercs@gmail.com>
2019-05-31 00:06:50 -0300

    MinimosQ e P3Q3

--- Codigos/MinimosQ.m
+++ Codigos/MinimosQ.m
@@ -1,7 +1,8 @@
-function [b0,b1] = MinimosQ(x,y)
+function [U, V, b0, b1] = MinimosQ(x,y)
 
 n = length(x);
 b1 = (sum(x)*sum(y)-n*sum(x.*y))/((sum(x)).^2-n*sum(x.^2));
 b0 = (sum(y)-b1*sum(x))/n;
-
+U = b0+b1*x;
+V = U/n-2;
 endfunction
--- Codigos/P3Q3.m
+++ Codigos/P3Q3.m
@@ -5,13 +5,18 @@ x = [ 183 173 168 188 158 163 193 163 178 ];
 %Peso
 y = [ 79 69 70 81 61 63 79 71 73 ];
 plot(x,y,'o');
+%plot(y,x,'o');
 hold on, grid on;
-[mqb1, m1b2] = MinimosQ(x,y);
-plot(x,mqb1+m1b2*x,'-m');
-title("Diagrama de Dispersão");
+U = MinimosQ(x,y);
+plot(x,U,'-m');
+%U = MinimosQ(y,x);
+%plot(y,U,'-m');
+hold on;
+
+title("Diagrama de Dispers�o");
 xlabel("Altura (cm)");
 ylabel("Peso (kg)");
-legend({"Pessoa", "mqb1+m1b2*x"}, "location", "east");
+legend({"Pessoa", "U = b1+b2*x"}, "location", "east");
 legend show;
 %A dispersãpo dos pontos se mostrou forte pela proximidade dos pontos
 %O Gráfico de Dispersão demonstrou uma correlação positiva, com uma aglomeração crescente ou seja, quanto mais alto mais pesado
