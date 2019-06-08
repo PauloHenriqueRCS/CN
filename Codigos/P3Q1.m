66c6bf6abdebed45c7d3d1b3e160df97cc2037b8
Paulo <paulohenriquercs@gmail.com>
2019-06-07 23:29:19 -0300

    Questao 1

--- Codigos/P3Q1.m
+++ Codigos/P3Q1.m
@@ -2,10 +2,22 @@ function [result] = P3Q1()
 clc, clear all;
 x = [.4 1.25 2.71 3.5];
 y = x.*sin(pi.*x);
-%y = 2.5;
+n = length(x);
+m = length(y);
+p = 2.5;
 
+P1 = InterL([x(1) x(2)],[y(1) y(2)],2.5)
+P1 = PLagrange([x(1) x(2)],[y(1) y(2)],2.5)
+P1 = PNewton([x(1) x(2)],[y(1) y(2)],2.5)
 
+P2 = InterQ([x(1) x(2) x(3)],[y(1) y(2) y(3)],2.5)
+P2 = PLagrange([x(1) x(2) x(3)],[y(1) y(2) y(3)],2.5)
+P2 = PNewton([x(1) x(2) x(3)],[y(1) y(2) y(3)],2.5)
 
-R = PLagrange(x,y,x)
+P3 = PLagrange(x,y,2.5)
+P3 = PNewton(x,y,2.5)
+% garrei no spline
+P3 = SplineA(n,x,y,1,2.5)
+
 
 endfunction
