f8d9690f1cd74a05187804e504dde5afa45805cf
aluno <aluno>
2019-06-10 10:14:20 -0300

    P3Q1 Metade

--- Codigos/P3Q1.m
+++ Codigos/P3Q1.m
@@ -1,23 +1,29 @@
 function [result] = P3Q1()
 clc, clear all;
-x = [.4 1.25 2.71 3.5];
+x = [0.4 1.25 2.71 3.5];
 y = x.*sin(pi.*x);
-p = 2.5;
+p = [0.4 1.25 2.5 2.71 3.5];
 
 format long;
 plot(x,y,'-m');
 hold on, grid on;
 
-PN(1) = PNewton([x(1) x(2)],[y(1) y(2)],2.5);
-PN(2) = PNewton([x(1) x(2) x(3)],[y(1) y(2) y(3)],2.5);
-PN(3) = PNewton(x,y,2.5);
+PN1 = PNewton([x(1) x(2)],[y(1) y(2)],p);
+PN2 = PNewton([x(1) x(2) x(3)],[y(1) y(2) y(3)],p);
+PN3 = PNewton(x,y,p);
 
-plot(x,PN(1),'-b');
+x = [0.4 1.25 2.5 2.71 3.5];
+
+plot(x,PN1,'-b');
+hold on;
+plot(x,PN2,'-g');
+hold on;
+plot(x,PN3,'-r');
 
 title("Polinomios");
 xlabel("x");
 ylabel("f(x) = xsen(pi*x)");
-legend({"f(x) = xsen(pi*x)","P1"}, "location", "east");
+legend({"f(x) = xsen(pi*x)","P1","P2","P3"}, "location");
 legend show;
 
 endfunction
